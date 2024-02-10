5ff7609651ee48c9cbf77baa1623daddd241b924
Om136 <202251154@iiitvadodara.ac.in>
2024-02-10 05:54:11 +0530

    corrected branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,21 +3,6 @@ import Home from "./Pages/Home";
 import Navbar from "./components/Navbar";
 import LoginPage from "./Pages/Login";
 import SignUpPage from "./Pages/SignUp";
-<<<<<<< HEAD
-import Community from "./Pages/Community";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/mycommunuties" element={<Community />} />
-      </Routes>
-    </BrowserRouter>
-=======
 import Footer from "./components/Footer";
 
 
@@ -28,7 +13,6 @@ function App() {
     <Outlet />
     <Footer></Footer>
     </div>
->>>>>>> d07e508ad20c6ed1771d3a38e312ae35e228cff9
   );
 }
 
